6cf547d5615f4548a6dd14953ad31041435aa847
azesm <azesm@yahoo.com>
2023-12-07 11:25:25 +0300

    3

--- first/first.py
+++ first/first.py
@@ -1,7 +1,43 @@
-print('Hello Python!')
+# print('Hello Python!')
+#
+# print(125 + 7)
+#
+# qwerty = float(input('Число?:'))
+# asdf = qwerty / 123
+# print(f'{asdf:.2f}')
 
-print(125 + 7)
+#3. Переменные, оператор присваивания, функции type и id
 
-qwerty = float(input('Число?:'))
-asdf = qwerty / 123
-print(f'{asdf:.2f}')
+# a = 7
+# print(a)
+#
+# a, b = 0, 12
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+#
+# print( id(a), id(b))
+
+q = 5.8
+w = 'Hello'
+
+print(type(q), type(w))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
