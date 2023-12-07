500d37673e6ae8c7f864c97b5a39c6cf2e5aa346
azesm <azesm@yahoo.com>
2023-12-07 21:58:10 +0300

    math

--- first/first.py
+++ first/first.py
@@ -18,13 +18,21 @@
 #
 # print( id(a), id(b))
 
-q = 5.8
-w = 'Hello'
+# q = 5.8
+# w = 'Hello'
+#
+# print(type(q), type(w))
 
-print(type(q), type(w))
+# var_a = 5
 
+# d = int(input())
+# print(abs(d))
 
+# d1, d2, d3, d4, d5 = map(int, input().split())
+# print(min(d1, d2, d3, d4, d5))
 
+d1, d2, d3, d4, d5 = map(int, input().split())
+print(min(d1, d2, d3, d4, d5))
 
 
 
